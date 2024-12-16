195c1526eec906fb440a4684364d3f1a36a069cf
Nijjwal Shrestha <Nijjwal Shrestha@DESKTOP-BN6MRAN.hsd1.nm.comcast.net>
2024-12-15 20:24:17 -0700

    Demo-7: Delete with @RequestParam

--- spring-boot-jpa/src/main/java/com/example/demo/controller/StudentController.java
+++ spring-boot-jpa/src/main/java/com/example/demo/controller/StudentController.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Student;
@@ -52,6 +54,11 @@ public class StudentController {
 		Student student = studentService.updateStudent(updateStudentRequest);
 
 		return new StudentResponse(student);
+	}
+
+	@DeleteMapping("delete")
+	public String deleteStudent(@RequestParam long id) {
+		return studentService.deleteStudent(id);
 	}
 
 }
--- spring-boot-jpa/src/main/java/com/example/demo/service/StudentService.java
+++ spring-boot-jpa/src/main/java/com/example/demo/service/StudentService.java
@@ -21,8 +21,8 @@ public class StudentService {
 	}
 
 	/**
-	 * Take Payload/request-dto from user to create a Student Entity and 
-	 * then store the entity in DB
+	 * Take Payload/request-dto from user to create a Student Entity and then store
+	 * the entity in DB
 	 * 
 	 * @param createStudentRequest
 	 */
@@ -36,23 +36,22 @@ public class StudentService {
 		return student;
 	}
 
-
 	/**
-	 * Takes DTO passed by controller, updates the data in database, 
-	 * and returns the updated record back to controller.
+	 * Takes DTO passed by controller, updates the data in database, and returns the
+	 * updated record back to controller.
+	 * 
 	 * @param updateStudentRequest
 	 * @return
 	 */
 	public Student updateStudent(UpdateStudentRequest updateStudentRequest) {
-		
-		//1. First, fetch student from the database
-		//findById uses primary key and returns Optional, so call get()
+
+		// 1. First, fetch student from the database
+		// findById uses primary key and returns Optional, so call get()
 		Student student = studentRepository.findById(updateStudentRequest.getId()).get();
 
-		//2. Second, iff the value is passed by user in payload, then
-		//set set that value in the student object we retrieved from DB.
-		if(updateStudentRequest.getFirstName()!=null && 
-		!updateStudentRequest.getFirstName().isEmpty()){
+		// 2. Second, iff the value is passed by user in payload, then
+		// set set that value in the student object we retrieved from DB.
+		if (updateStudentRequest.getFirstName() != null && !updateStudentRequest.getFirstName().isEmpty()) {
 			student.setFirstName(updateStudentRequest.getFirstName());
 		}
 
@@ -61,5 +60,12 @@ public class StudentService {
 		return student;
 	}
 
+	public String deleteStudent(long id) {
+		//deleteById method doesn't return anything. But if doesn't 
+		//throw any error we will just return a string msg.
+		studentRepository.deleteById(id);
+
+		return "Student has been deleted successfully!";
+	}
 
 }
