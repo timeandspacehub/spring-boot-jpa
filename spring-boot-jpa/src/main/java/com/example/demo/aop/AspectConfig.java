0bf36f22b5ec2d336939011ac0c6aec80df2e0d3
Nijjwal Shrestha <nijjwal.shrestha@gmail.com>
2024-12-31 18:14:51 -0700

    Demo-32: @After Advice - runs before return statement

--- spring-boot-jpa/src/main/java/com/example/demo/aop/AspectConfig.java
+++ spring-boot-jpa/src/main/java/com/example/demo/aop/AspectConfig.java
@@ -1,8 +1,8 @@
 package com.example.demo.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -33,7 +33,15 @@ public class AspectConfig {
 	//Here we have changed the pointcut - i.e. which method to intercept
 	//The value passed into the method parameter will now be assigned to "object" defined below. 
 	//You can access the object by declaring object. Parameter names MUST match! 
+	/**
 	@Before(value = "execution(* com.example.demo.controller.*.*(..)) and args(object)")
+	public void beforeAdvice(JoinPoint joinpoint, Object object) {
+		logger.info("Request= " + object);
+		
+	}
+	*/
+
+	@After(value = "execution(* com.example.demo.controller.*.*(..)) and args(object)")
 	public void beforeAdvice(JoinPoint joinpoint, Object object) {
 		logger.info("Request= " + object);
 		
